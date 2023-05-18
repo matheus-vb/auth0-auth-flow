c7be4f75933c1850e79155a17d2647c3245350a2
Matheus Batista <matheus.vbatista04@gmail.com>
2023-05-18 00:14:19 -0300

    chore: add comments

--- AuthClient/AuthClient/MainView.swift
+++ AuthClient/AuthClient/MainView.swift
@@ -20,7 +20,7 @@ extension MainView {
     func login() {
         Auth0
             .webAuth()
-            .audience("micro-elearning-auth")
+            .audience("micro-elearning-auth") //Auth0 Dashboard -> API audience id
             .start { result in
                 switch result {
                 case .success(let credentials):
@@ -45,3 +45,21 @@ extension MainView {
             }
     }
 }
+
+//To link with Auth0 application, create Auth0.plist file:
+
+//ClienId and Application domain from Auth0 dashboard -> application
+
+//Auth0.plist
+
+//<?xml version="1.0" encoding="UTF-8"?>
+//<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+//<plist version="1.0">
+//<dict>
+//    <key>ClientId</key>
+//    <string>ClientID</string>
+//    <key>Domain</key>
+//    <string>ApplicationDomain</string>
+//</dict>
+//</plist>
+
