4dd74975c538da44cf89862e9ea83b66db552a45
Matheus Batista <matheus.vbatista04@gmail.com>
2023-05-18 00:10:26 -0300

    feature: add login to app

---
+++ AuthClient/AuthClient/MainView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+import Auth0
+
+struct MainView: View {
+    @State var user: User?
+
+    var body: some View {
+        if let user = self.user {
+            VStack {
+                ProfileView(user: user)
+                Button("Logout", action: self.logout)
+            }
+        } else {
+            Button("Login", action: self.login)
+        }
+    }
+}
+
+extension MainView {
+    func login() {
+        Auth0
+            .webAuth()
+            .audience("micro-elearning-auth")
+            .start { result in
+                switch result {
+                case .success(let credentials):
+                    self.user = User(from: credentials.idToken)
+                    print("||->\(credentials.accessToken)<-||")
+                case .failure(let error):
+                    print("Failed with: \(error)")
+                }
+            }
+    }
+
+    func logout() {
+        Auth0
+            .webAuth()
+            .clearSession { result in
+                switch result {
+                case .success:
+                    self.user = nil
+                case .failure(let error):
+                    print("Failed with: \(error)")
+                }
+            }
+    }
+}
